dd8e111dd09e84f08fa38df58b0ac484d41af741
Beau <beau@beaudesigns.net>
2011-07-13 08:24:55 -0500

    Reverted back to the old way of fetching tweets. It doesn't work across domains.

--- base.js
+++ base.js
@@ -1,26 +1,20 @@
 window.addEvent('domready', function() {
-	fetchTwitterUpdate();
-	addTwitterSectionClick();
-});
-
-function fetchTwitterUpdate() {
-	new Request.JSON({
-		url: 'https://twitter.com/statuses/user_timeline/beaudesigns.json?count=1',
-		onSuccess: function(tweets) {
-			twitterUpdate(tweets[0].text);
-		}
-	}).get();
-}
-
-function twitterUpdate(tweet) {
-	$('recent_tweet').set('text', tweet);
-}
-
-function addTwitterSectionClick() {
 	$('twitter').addEvent('click', function(event) {
 		var href = this.getElement('a').get('href');
 		if(href) {
 			window.location = href;
 		}
 	});
+});
+
+function tweetBack(JSON) {
+	if(typeOf(JSON) == 'array') {
+		window.addEvent('domready', function() {
+			twitterUpdate(JSON[0].text);
+		});
+	}
+}
+
+function twitterUpdate(tweet) {
+	$('recent_tweet').set('text', tweet);
 }
