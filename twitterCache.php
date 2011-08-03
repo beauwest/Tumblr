320e8d4f4d86abf7d5fe8ef33bf1d0351746e4fe
Beau <beau@beaudesigns.net>
2011-08-03 10:15:43 -0500

    Another attempt.

--- twitterCache.php
+++ twitterCache.php
@@ -28,7 +28,7 @@ class TwitterCache
 		}
 		
 		$Tweets = json_decode(file_get_contents($this->Options['Path']));
-		if(!empty($Tweets[0])) {
+		if(is_array($Tweets)) {
 			$this->SendTweetCallback($Tweets[0]->text);
 		} else {
 			unlink($this->Options['Path']);
